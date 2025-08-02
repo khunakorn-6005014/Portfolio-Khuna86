bec659cf215a5a1425abc4145260f05566d1dce1
khuna <khunakorn.w@gmail.com>
2025-08-02 01:53:14 -0700

    update next and  package

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Enable static HTML export
+  output: 'export',
+
+  // other existing config options…
 };
 
 export default nextConfig;

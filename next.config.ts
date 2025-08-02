b3c315c0e3faab7145c9a7cde004b9dcfa3c385b
khuna <khunakorn.w@gmail.com>
2025-08-02 02:55:49 -0700

    update next2

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",        // → Generates pure HTML/CSS/JS into `out/`
-  trailingSlash: true,     // → Adds a slash to every route (e.g. `/about/`)
+  //trailingSlash: true,     // → Adds a slash to every route (e.g. `/about/`)
   images: {
     unoptimized: true      // → Bypasses Next.js image optimization
   }
